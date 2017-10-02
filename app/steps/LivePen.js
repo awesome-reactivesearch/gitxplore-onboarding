558f650a31f26eb663b302ae4486823247572858
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-10-02 13:13:49 +0530

    Update LivePen props

--- app/steps/LivePen.js
+++ app/steps/LivePen.js
@@ -7,9 +7,9 @@ const LivePen = () => (
 			scrolling="no"
 			title="GitXplore final app"
 			src="https://codepen.io/divyanshu013/embed/pwZxjN/?height=1124&theme-id=30160&default-tab=result&embed-version=2"
-			frameborder="no"
-			allowtransparency="true"
-			allowfullscreen="true"
+			frameBorder="no"
+			allowTransparency="true"
+			allowFullScreen="true"
 			style={{width: '100%', height: 900}}
 		>
 			See the Pen <a href="https://codepen.io/divyanshu013/pen/pwZxjN/">GitXplore final app</a> by Divyanshu (<a href="https://codepen.io/divyanshu013">@divyanshu013</a>) on <a href="https://codepen.io">CodePen</a>.
